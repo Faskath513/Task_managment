20fd55858dae2da1988a30ca66ef44a6daee1b0c
Faskath513 <faskath9054@gmail.com>
2024-10-08 07:33:34 +0530

    Added TaskList and TaskDetail views for task management

--- task_manager/api/views.py
+++ task_manager/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    def retrieve(self, request, *args, **kwargs):
+        task = self.get_object()
+        if task.user != request.user:
+            raise PermissionDenied("You do not have permission to access this task.")
+        return super().retrieve(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        task = self.get_object()
+        if task.user != request.user:
+            raise PermissionDenied("You do not have permission to update this task.")
+        return super().update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        task = self.get_object()
+        if task.user != request.user:
+            raise PermissionDenied("You do not have permission to delete this task.")
+        return super().destroy(request, *args, **kwargs)
